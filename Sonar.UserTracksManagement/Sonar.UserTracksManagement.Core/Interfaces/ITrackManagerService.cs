82165a691c0323c112832dc16ac2c179dd55148f
Serelllka <vprogamervip@gmail.com>
2022-05-28 01:22:43 +0300

    feat: adds TrackManagerServise

---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Interfaces/ITrackManagerService.cs
@@ -0,0 +1,10 @@
+﻿using Sonar.UserTracksManagement.Core.Entities;
+
+namespace DefaultNamespace;
+
+public interface ITrackManagerService
+{
+    IEnumerable<Track> GetAvailableTracks(User user);
+    bool CheckAvailability(User user, Track track);
+    void AddTrackToUser(User user, Track track);
+}
---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Services/TrackManagerService.cs
@@ -0,0 +1,21 @@
+﻿using Sonar.UserTracksManagement.Core.Entities;
+
+namespace DefaultNamespace;
+
+public class TrackManagerService : ITrackManagerService
+{
+    public IEnumerable<Track> GetAvailableTracks(User user)
+    {
+        return user.Tracks;
+    }
+
+    public bool CheckAvailability(User user, Track track)
+    {
+        return GetAvailableTracks(user).Contains(track);
+    }
+
+    public void AddTrackToUser(User user, Track track)
+    {
+        user.AddTrack(track);
+    }
+}
